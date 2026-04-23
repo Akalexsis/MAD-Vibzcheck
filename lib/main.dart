91ab5bc55569d9de497443bafdd18e3db2299ee6
Kayla Thornton <kaylathornton@Kaylas-MacBook-Air.local>
2026-04-23 17:53:15 -0400

    bug-fix - fix firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-	WidgetFlutterBinding.ensureInitialized();
-	await firebase.initializeApp( options: DefaultFirebaseOptions.currentPlatform, );
-	
-	run(const MyApp());
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
