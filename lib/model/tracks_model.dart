e3e1672b3b68afd5bff4517f03d668f783ddf65f
Kayla Thornton <kaylathornton@Kaylas-Air.wifi.gsu.edu>
2026-04-27 14:36:13 -0400

    initialize - create tracks model to get songs from spotify and save into database

---
+++ lib/model/tracks_model.dart
@@ -0,0 +1,37 @@
+/* 
+    Author - Kayla Thornton
+    Purpose - Pull tracks from Spotify API and store them in Firebase database
+*/
+
+class TracksModel {
+    // create track object
+    final int? id;
+    final String name;
+    final String artist;
+    final int votes;
+
+    TracksModel({
+        this.id,
+        required this.name,
+        required this.artist,
+        this.votes = 0 // default value
+    });
+
+    // convert object to track record to upload into database
+    Map<String, dynamic> toMap() => {
+        'id': id,
+        'name': name,
+        'artist': artist,
+        'votes': votes
+    };
+
+    // convert track record to object
+    factory TracksModel.fromMap() => TracksModel(
+        id: id,
+        name: map['name'],
+        artist: map['artist'],
+        votes: map['votes']
+    )
+
+    // convert json to track object to be used in database
+}
